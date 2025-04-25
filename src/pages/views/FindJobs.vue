db319772032f8079760636d6b6edb0ec0fda7b91
Saren Sokmeak <saroussokmeak721@gmail.com>
2025-04-25 13:20:13 +0700

    We are now starting develop the Landing page:

--- src/pages/views/FindJobs.vue
+++ src/pages/views/FindJobs.vue
@@ -35,13 +35,5 @@
       </div>
     </div>
   </div>
-  <div class="container">
-    <h1>Find Jobs Page</h1>
-    <div class="row">
-      <div class="col-md-12">
-        <h2>Find Jobs</h2>
-        <p>Explore job opportunities that match your skills and interests.</p>
-      </div>
-    </div>
-  </div>
+  
 </template>
