ea329aa26346ee3ec6b33f92eae0631431fbcf54
Saren Sokmeak <saroussokmeak721@gmail.com>
2025-06-24 02:06:05 +0700

    update app

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,4 @@
   <RouterView />
 </template>
 
-<script setup>
-// Initialization logic (optional)
-const initApp = async () => {
-  try {
-    if (!document.getElementById("toast-container")) {
-      const toastContainer = document.createElement("div");
-      toastContainer.id = "toast-container";
-      toastContainer.style.zIndex = "9500";
-      document.body.appendChild(toastContainer);
-    }
-  } catch (error) {
-    console.error("App initialization failed:", error);
-  }
-};
-
-initApp();
-</script>
+<script setup></script>
